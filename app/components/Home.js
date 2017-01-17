8b1c17dfc7462c032d739181f94e6fba9994818a
musale <martinmshale@gmail.com>
2017-01-17 11:49:17 +0300

    added the battle landing page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,18 @@
 var React = require('react');
-
+var ReactRouter = require('react-router');
+var transparentBg = require('../styles').transparentBg;
+var Link = ReactRouter.Link;
 var Home = React.createClass({
   render: function () {
     return (
-      <div> Hello from Home! </div>
+      <div className="jumbotron text-center" style={transparentBg}>
+            <h1>Github Battle</h1>
+            <p className="lead">my motto is coming soon</p>
+
+            <Link to="/playerOne">
+                <button type="button" className="btn btn-lg btn-success">Get Started</button>
+            </Link>
+      </div>
     )
   }
 });
