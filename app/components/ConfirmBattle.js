ce89eff421282ab0f65070fae909798ee3cbb841
musale <martinmshale@gmail.com>
2017-01-19 11:00:46 +0300

    changed the initiateBattle function name

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,15 +5,12 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function puke (obj) {
-    return <pre>{JSON.stringify(obj, null, " ")}</pre>
-}
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle (props) {
     return props.isLoading === true
         ? <p> Loading</p>
-        : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        : <MainContainer>
             <h1>Confirm Players</h1>
             <div className='col-sm-8 col-sm-offset-2'>
                 <UserDetailsWrapper header='Player 1'>
@@ -33,13 +30,13 @@ function ConfirmBattle (props) {
                 </Link>
             </div>
             </div>
-            </div>
+            </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
     isLoading: PropTypes.bool.isRequired,
     playersInfo: PropTypes.array.isRequired,
-    onInititateBattle: PropTypes.func.isRequired
+    onInitiateBattle: PropTypes.func.isRequired
 }
 
 module.exports = ConfirmBattle;
